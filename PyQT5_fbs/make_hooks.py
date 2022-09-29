d9b489e3721b1de5cf7af10cfdd376f339e6752d
Nemanja Komar <komar@hawaii.edu>
2022-09-28 19:37:12 -1000

    Print if path exists

--- PyQT5_fbs/make_hooks.py
+++ PyQT5_fbs/make_hooks.py
@@ -17,14 +17,15 @@ datas = collect_data_files('utide')
 
 
     for path in site.getsitepackages():
-        print("Writing to",path)
         destination = os.path.join(path, 'PyInstaller', 'hooks')
         if os.path.exists(destination):
+            print("Writing to", destination)
             with open(os.path.join(destination, fname), 'w') as f:
                 f.write('{}'.format(data).strip())
 
             assert os.path.exists(os.path.join(destination, fname))
-
+        else:
+            print("This path does not exist", destination)
 
 if __name__ == '__main__':
     main()
